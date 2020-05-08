7407096097a9be1796c83c0f7bce1a299014bb0f
llanos <llanos1205@gmail.com>
2020-05-08 15:48:09 -0400

    .

--- layers/lambdaPackagePostgres/responses.py
+++ layers/lambdaPackagePostgres/responses.py
@@ -1,7 +1,9 @@
 import json
-def Response(StatusCode,Body,hdrs:dict={ "Content-Type": "application/json"}):
-    return{
-        'statusCode':StatusCode,
-        'body':json.dumps(Body),
-        'headers':hdrs
+
+
+def Response(StatusCode, Body, hdrs: dict = {"Content-Type": "application/json"}):
+    return {
+        'statusCode': StatusCode,
+        'body': json.dumps(Body),
+        'headers': hdrs
     }
---
+++ layers/lambdaPackagePostgres/scripts.py
@@ -0,0 +1,5 @@
+#generic queries
+get_query = "Select {} from {};"
+put_query = "insert into {}({}) values({});"
+update_query = "Update {} set {} where {};"
+delete_query = "delete from {} where {}"
